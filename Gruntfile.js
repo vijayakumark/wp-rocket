3b5caf360455e734abc31f55562c3f2b365877b2
fxbenard <fxbenard@gmail.com>
2014-09-20 16:24:13 +0200

    Update grunt files to wp-i18n 0.4.8 - include Poedit header now

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,19 +14,17 @@ grunt.initConfig({
           exclude: ['build/.*'],
           mainFile: 'wp-rocket.php',    // Main project file.
           potFilename: 'rocket.pot',    // Name of the POT file.
+          potHeaders: {
+                    poedit: true,                 // Includes common Poedit headers.
+                    'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
+                },
           type: 'wp-plugin',    // Type of project (wp-plugin or wp-theme).
           updateTimestamp: true,    // Whether the POT-Creation-Date should be updated without other changes.
           processPot: function( pot, options ) {
             pot.headers['report-msgid-bugs-to'] = 'http://wp-rocket.me/';
-            pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
             pot.headers['last-translator'] = 'WP Media (http://wp-rocket.me/)\n';
             pot.headers['language-team'] = 'WP Media (http://www.transifex.com/projects/p/wp-media/)\n';
-            pot.headers['x-poedit-basepath'] = '.\n';
-            pot.headers['x-poedit-language'] = 'English\n';
-            pot.headers['x-poedit-country'] = 'UNITED STATES\n';
-            pot.headers['x-poedit-sourcecharset'] = 'utf-8\n';
-            pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_  noop:1,2;_n_noop:1,2;_c,_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
-            pot.headers['x-textdomain-support'] = 'yes\n';
+            pot.headers['language'] = 'en_US';
             return pot;
           }
         }
