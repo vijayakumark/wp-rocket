24707ccffd26a088f453ad1b459c1124f5129e91
fxbenard <fxbenard@gmail.com>
2015-03-10 14:14:11 +0100

    More grunt love - checktextdomain

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,41 @@ grunt.initConfig({
 				}
 			},
 
+		checktextdomain: {
+			options:{
+				text_domain: 'rocket',
+				create_report_file: true,
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,3d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d',
+					' __ngettext:1,2,3d',
+					'__ngettext_noop:1,2,3d',
+					'_c:1,2d',
+					'_nc:1,2,4c,5d'
+					]
+				},
+				files: {
+					src: [
+						'**/*.php', // Include all files
+						'!node_modules/**', // Exclude node_modules/
+						'!build/.*'// Exclude build/
+						],
+					expand: true
+				}
+			},
+
 		exec: {
 			txpull: { // Pull Transifex translation - grunt exec:txpull
 				cmd: 'tx pull -a --minimum-perc=89' // Change the percentage with --minimum-perc=yourvalue
