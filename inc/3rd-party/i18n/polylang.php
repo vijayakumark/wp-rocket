bb7cb9da140d4a48f6ea437cb8de9d7e60804d05
Rémi Perona <remperona@gmail.com>
2016-02-11 21:20:51 -0500

    code improvements

--- inc/3rd-party/i18n/polylang.php
+++ inc/3rd-party/i18n/polylang.php
@@ -10,14 +10,10 @@ if( defined( 'POLYLANG_VERSION' ) && POLYLANG_VERSION ) :
  */
 add_action( 'after_rocket_clean_domain', '_rocket_force_clean_domain_on_polylang' );
 function _rocket_force_clean_domain_on_polylang() {
-    if ( POLYLANG_VERSION < '1.8' ) {
-        if ( isset( $GLOBALS['polylang'] ) && 0 === $GLOBALS['polylang']->options['force_lang'] ) {
-            rocket_clean_cache_dir();
-        }
-    } else {
-        if ( function_exists( 'PLL' ) && 0 === PLL()->options['force_lang'] ) {
-            rocket_clean_cache_dir();
-        }
+    if ( function_exists( 'PLL' ) && isset( PLL() ) && 0 === PLL()->options['force_lang'] ) {
+        rocket_clean_cache_dir();
+    } else if ( isset( $GLOBALS['polylang'] ) && 0 === $GLOBALS['polylang']->options['force_lang'] ) {
+        rocket_clean_cache_dir();
     }
 }
 
