1782e394547cecda62d0ce719a6ef015edfefe90
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-02-15 12:02:44 +0100

    Improvement: Compatibility with "EU Cookie Law" plugin

--- inc/3rd-party/3rd-party.php
+++ inc/3rd-party/3rd-party.php
@@ -16,6 +16,7 @@ require( WP_ROCKET_3RD_PARTY_PATH . 'i18n/wpml.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'i18n/polylang.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'aelia-currencyswitcher.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'age-verify.php' );
+require( WP_ROCKET_3RD_PARTY_PATH . 'eu-cookie-law.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'kk-star-ratings.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'wp-postratings.php' );
 require( WP_ROCKET_3RD_PARTY_PATH . 'wp-print.php' );
