1cf9d3876dd81d683be6974a181828bc8680b358
Rémi Perona <remperona@gmail.com>
2016-02-16 20:07:15 -0500

    Code improvement on AMP function

--- inc/3rd-party/mobile/amp.php
+++ inc/3rd-party/mobile/amp.php
@@ -9,16 +9,15 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
  *
  */
 if ( defined( 'AMP_QUERY_VAR' ) && function_exists( 'is_amp_endpoint' ) ) {
-    add_action( 'wp', '_rocket_amp' );
+    add_action( 'wp', '_rocket_disable_options_on_amp' );
 }
 
-function _rocket_amp() {
+function _rocket_disable_options_on_amp() {
     if ( is_amp_endpoint() ) {
         remove_filter( 'rocket_buffer', 'rocket_exclude_deferred_js', 11 );
         remove_filter( 'rocket_buffer', 'rocket_dns_prefetch', 12 );
         remove_filter( 'rocket_buffer', 'rocket_minify_process', 13 );
-        remove_filter( 'rocket_buffer', 'rocket_lazyload_iframes', PHP_INT_MAX );
-        remove_filter( 'get_avatar',    'rocket_lazyload_images', PHP_INT_MAX );
-        remove_filter( 'the_content',   'rocket_lazyload_images', PHP_INT_MAX );
+
+        add_filter( 'do_rocket_lazyload', '__return_false' );
     }
 }
