134fd4a29caa84a8e1550b2634636d17ac1cab74
Jonathan Buttigieg <jonathan.buttigieg@yahoo.fr>
2016-02-25 12:05:40 +0100

    Remove the option "Disable the HTML caching files generated by WP Rocket"

--- inc/admin/upgrader.php
+++ inc/admin/upgrader.php
@@ -110,7 +110,6 @@ function rocket_first_install() {
 			'cloudflare_auto_settings'    => 0,
 			'cloudflare_old_settings'     => 0,
 			'varnish_auto_purge'          => 0,
-			'do_caching_files'            => 0,
 			'do_beta'                     => 0,
 		)
 	);
