2fa5a4065a6fce8a77d24104bf430ae5f0e55603
Rémi Perona <remperona@gmail.com>
2016-02-28 07:43:28 -0500

    keep actions with print_emoji_styles() for styling of default WP smileys

--- inc/common/emoji.php
+++ inc/common/emoji.php
@@ -13,9 +13,7 @@ if ( version_compare( $wp_version, '4.2' ) >= 0 && get_rocket_option( 'emoji', 0
 add_action( 'init', '__rocket_disable_emoji' );
 function __rocket_disable_emoji() {
 	remove_action( 'wp_head'				, 'print_emoji_detection_script', 7 );
-	remove_action( 'admin_print_scripts'	, 'print_emoji_detection_script' );
-	remove_action( 'wp_print_styles'		, 'print_emoji_styles' );
-	remove_action( 'admin_print_styles'		, 'print_emoji_styles' );	
+	remove_action( 'admin_print_scripts'	, 'print_emoji_detection_script' );	
 	remove_filter( 'the_content_feed'		, 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss'		, 'wp_staticize_emoji' );	
 	remove_filter( 'wp_mail'				, 'wp_staticize_emoji_for_email' );
